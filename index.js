c3c893305fe514c7f499c2ba4b39b545faea60aa
Ricky Thakar <rickythakar@gmail.com>
2022-04-20 21:06:35 -0400

    created function to write readme file and created questions

--- index.js
+++ index.js
@@ -5,30 +5,79 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is ',
+
+    },
     
+
 ];
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            console.log(err);}
+    }
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
